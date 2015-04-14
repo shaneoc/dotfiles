efbc565318702601ff5740b3dcfa04642c202e9a
Shane O'Connell <shane@oconnell.cc>
2015-04-14 00:42:02 -0400

    added sc cd feature

--- scripts/sc-tab-completion.bashrc.sh
+++ scripts/sc-tab-completion.bashrc.sh
@@ -3,10 +3,10 @@ _sc ()
     local cur=${COMP_WORDS[$COMP_CWORD]}
     local cmd=${COMP_WORDS[0]}
     if [ $COMP_CWORD -eq 1 ]; then
-        COMPREPLY=($(compgen -W "$($cmd --names)" -- $cur))
+        COMPREPLY=($(compgen -W "cd $($cmd --names)" -- $cur))
     elif [ $COMP_CWORD -eq 2 ]; then
         local arg="${COMP_WORDS[1]}"
-        if [ "$arg" == "edit" -o "$arg" == "path" ]; then
+        if [ "$arg" == "cd" -o "$arg" == "edit" -o "$arg" == "path" ]; then
             COMPREPLY=($(compgen -W "$($cmd --names)" -- $cur))
         fi
 
@@ -18,6 +18,19 @@ _sc ()
     #     if [ $? -eq 0 ]; then
     #         COMPREPLY=($(compgen -W "$output" -- $cur))
     #     fi
+    fi
+}
+
+sc ()
+{
+    if [ "$1" == "cd" ]; then
+        if [ $# -lt 2 ]; then
+            echo "Usage: sc cd <script>"
+            return 1
+        fi
+        cd "$(dirname "$(command sc path "$2")")"
+    else
+        command sc "$@"
     fi
 }
 
